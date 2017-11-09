e691d4cbc9e9654ee102a86a12be7a5eb0392959
Colum <15142434@studentmail.ul.ie>
2017-11-09 12:55:05 +0000

    Fire House

--- outsidegen.cpp
+++ outsidegen.cpp
@@ -1,10 +1,8 @@
 #include "outsidegen.h"
-#include <QPainter>
-#include <QPoint>
-#include <QPen>
 
 
-outsideGen::outsideGen(House *  rm ,QWidget *parent) : QWidget(parent)
+
+outsideGen::outsideGen(House*  rm ,QWidget *parent) : QWidget(parent)
 {
     this->rm=rm;
      this->update();
@@ -14,12 +12,25 @@ void outsideGen::changeHouse(House* rm){
     this->update();
 }
 void outsideGen::paintEvent(QPaintEvent *e){
-     QPainter painter( this );
+    QPainter painter( this );
   QPixmap outsideA(":/resource/ROOMS/OutsideA.png");
   QPixmap outsideF(":/resource/ROOMS/OutsideF.png");
   QPixmap outsideD(":/resource/ROOMS/OutsideD.png");
+   QPixmap FireB(":/resource/ROOMS/FireB.png");
+   QPixmap FireC(":/resource/ROOMS/FireC.png");
+   QPixmap FireE(":/resource/ROOMS/FireE.png");
+   QPixmap FireG(":/resource/ROOMS/FireG.png");
+   QPixmap FireH(":/resource/ROOMS/FireH.png");
+   QPixmap FireI(":/resource/ROOMS/FireI.png");
+    this->FireB=FireB;
+    this->FireC=FireC;
+    this->FireE=FireE;
+    this->FireG=FireG;
+    this->FireH=FireH;
+    this->FireI=FireI;
+
   int squareSize = rect().width() < rect().height() ? rect().width() : rect().height();
-  QRect backgroundRect = QRect(0,0,squareSize,squareSize);
+  backgroundRect = QRect(0,0,squareSize,squareSize);
     if(rm->shortDescription()=="a")
      painter.drawPixmap(backgroundRect, outsideA);
     else if(rm->shortDescription()=="f")
@@ -27,4 +38,25 @@ void outsideGen::paintEvent(QPaintEvent *e){
     else if(rm->shortDescription()=="d")
         painter.drawPixmap(backgroundRect, outsideD);
 
+}
+void outsideGen::HouseFire(House* h){
+     QPainter painter( this );
+    if(h->shortDescription()=="b"){
+         painter.drawPixmap(backgroundRect, FireB);
+    }
+    else if(h->shortDescription()=="c"){
+           painter.drawPixmap(backgroundRect, FireC);
+    }
+    else if(h->shortDescription()=="e"){
+            painter.drawPixmap(backgroundRect, FireE);
+    }
+   else if(h->shortDescription()=="g"){
+        painter.drawPixmap(backgroundRect, FireG);
+    }
+    else if(h->shortDescription()=="i"){
+        painter.drawPixmap(backgroundRect, FireI);
+    }
+    else if(h->shortDescription()=="h"){
+        painter.drawPixmap(backgroundRect, FireH);
+    }
 }
--- outsidegen.h
+++ outsidegen.h
@@ -4,12 +4,24 @@
 #include <QWidget>
 #include <QGridLayout>
 #include "House.h"
+#include <QPainter>
+#include <QPoint>
+#include <QPen>
 class outsideGen : public QWidget
 {
     Q_OBJECT
 public:
     outsideGen(House *rm, QWidget *parent=0);
-     void changeHouse( House* rm);
+    void changeHouse( House* rm);
+    void HouseFire(House *h);
+private:
+      QPixmap FireB;
+      QPixmap FireC;
+      QPixmap FireE;
+      QPixmap FireG;
+      QPixmap FireH;
+      QPixmap FireI;
+      QRect backgroundRect;
 protected:
     House *rm;
     void paintEvent(QPaintEvent *e);
